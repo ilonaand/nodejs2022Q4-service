2a1adf99faf241346a9157696f03a6fd9503e3cb
ilonaand <ilonaand@gmail.com>
2023-02-20 01:10:53 +0300

    fix: app.modile

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,20 @@ import { FavoritesModule } from './favorites/favorites.module';
 import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    DatabaseModule,
     AlbumsModule,
     AlbumsModule,
     TracksModule,
     FavoritesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
